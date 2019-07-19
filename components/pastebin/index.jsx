27ae93a870fe94956be7116ccdee7eea1c77c6eb
Matthew Palmer <matt@silvercreekit.com>
2019-07-19 04:21:26 -0400

    start form submission

--- components/pastebin/index.jsx
+++ components/pastebin/index.jsx
@@ -3,14 +3,26 @@ import React, { useState } from 'react';
 
 export default function Pastebin() {
   const [contents, setContents] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setIsSubmitting(true);
+    setTimeout(() => {
+      setIsSubmitting(false);
+    }, 2000);
+  }
 
   return (
-    <form method="post" action="/">
+    <form method="post" action="/" onSubmit={handleSubmit}>
       <input
         value={contents}
+        disabled={isSubmitting}
         onChange={event => setContents(event.target.value)}
       />
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={isSubmitting}>
+        Submit
+      </button>
     </form>
   );
 }
