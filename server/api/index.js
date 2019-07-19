61f6e97dabb817fac05fc6fdb78ee428eb4b4c68
Matthew Palmer <matt@silvercreekit.com>
2019-07-19 06:06:44 -0400

    add comments to server entry point

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,6 @@
 
 module.exports = function api(server) {
   server.post('/', (req, res) => {
-    console.log(req.body);
     res.send('AY');
   });
 };
--- server/index.js
+++ server/index.js
@@ -5,22 +5,26 @@ const express = require('express');
 const next = require('next');
 const api = require('./api');
 
-
+// Initialize Next.js
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
 
 app
   .prepare()
   .then(() => {
+    // Initialize Express and apply middleware
     const server = express();
     server.use(express.json());
 
+    // Apply API routes
+    api(server);
+
+    // Default route handler
     server.get('*', (req, res) => {
       return handle(req, res);
     });
 
-    api(server);
-
+    // Start HTTP server
     server.listen(process.env.PORT, (error) => {
       if (error) throw error;
       console.log(`Server started on port ${process.env.PORT}`);
