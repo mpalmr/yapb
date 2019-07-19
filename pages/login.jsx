106a42feab635bca3b3c735422404490cd047135
Matthew Palmer <matt@silvercreekit.com>
2019-07-19 19:34:54 -0400

    move login and registration logic into page components

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
-import Login from '../components/login';
+import axios from 'axios';
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+} from 'formik';
+import { Container } from 'react-bootstrap';
+import validation from '../validation/login';
+
+
+const formConfig = {
+  validation,
+  initialValues: { email: '', password: '' },
+
+  async onSubmit(values) {
+    return axios
+      .post('/login', values)
+      .then((res) => {
+        console.log(res);
+        return res;
+      });
+  },
+};
 
 
 export default function LoginPage() {
   return (
-    <>
+    <Container>
       <h2>Login</h2>
-      <Login />
-    </>
+      <Formik {...formConfig}>
+        {({ isSubmitting }) => (
+          <Form method="post" action="/login">
+            <Field name="email" type="email" />
+            <ErrorMessage name="email" component="div" />
+
+            <Field name="password" type="password" />
+            <ErrorMessage name="password" component="div" />
+
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </Container>
   );
 }
--- pages/register.jsx
+++ pages/register.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
-import Registration from '../components/registration';
+import axios from 'axios';
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+} from 'formik';
+import { Container } from 'react-bootstrap';
+import validation from '../validation/registration';
+
+
+const formConfig = {
+  validation,
+  initialValues: { email: '', password: '', githubUrl: '' },
+
+  async onSubmit(values) {
+    return axios
+      .post('/register', values)
+      .then((res) => {
+        console.log(res);
+        return res;
+      });
+  },
+};
 
 
 export default function RegisterPage() {
   return (
-    <>
+    <Container>
       <h2>Register</h2>
-      <Registration />
-    </>
+      <Formik {...formConfig}>
+        {({ isSubmitting }) => (
+          <Form method="post" action="/register">
+            <Field name="email" type="email" />
+            <ErrorMessage name="email" component="div" />
+
+            <Field name="password" type="password" />
+            <ErrorMessage name="password" component="div" />
+
+            <Field name="githubUrl" />
+            <ErrorMessage name="githubUrl" component="div" />
+
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </Container>
   );
 }
