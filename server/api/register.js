249b9d283e12692ee778f055bf430d960a4647a0
Matthew Palmer <matt@silvercreekit.com>
2019-07-19 12:09:59 -0400

    registration validation and organization

--- server/api/register.js
+++ server/api/register.js
@@ -3,18 +3,41 @@
 const argon2 = require('argon2');
 
 
-module.exports = function registerRoute({ db }) {
-  return async (req, res, next) => argon2
-    .hash(req.body.password)
-    .then(passwordHash => db('users')
-      .insert({
-        email: req.body.email,
-        password: passwordHash,
-        github_url: req.body.githubUrl,
-      })
-      .then((result) => {
-        res.json(result);
-        return result;
-      })
-      .catch(next));
+module.exports = function registerRoute({ server, db }) {
+  function validate(req, res, next) {
+    const errors = {};
+
+    if (!req.body.email) errors.email = 'Required';
+
+    if (!req.body.password) errors.password = 'Required';
+    else if (req.body.password.length < 6) {
+      errors.password = 'Password must be at least six characters';
+    }
+
+    if (Object.keys(errors).length) {
+      res
+        .status(400)
+        .json({ errors });
+    } else next();
+  }
+
+
+  async function createUser(req, res, next) {
+    return argon2
+      .hash(req.body.password)
+      .then(passwordHash => db('users')
+        .insert({
+          email: req.body.email,
+          password: passwordHash,
+          github_url: req.body.githubUrl,
+        })
+        .then((result) => {
+          res.json(result);
+          return result;
+        })
+        .catch(next));
+  }
+
+
+  server.post('/register', validate, createUser);
 };
