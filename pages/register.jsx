e50dc4d43ca68e4bb6340f7e50d49a407df3e850
Matthew Palmer <matt@silvercreekit.com>
2019-07-19 20:16:13 -0400

    registration

--- pages/register.jsx
+++ pages/register.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import axios from 'axios';
+import * as yup from 'yup';
+import { Formik } from 'formik';
 import {
-  Formik,
+  Container,
   Form,
-  Field,
-  ErrorMessage,
-} from 'formik';
-import { Container } from 'react-bootstrap';
-import validation from '../validation/registration';
+  Button,
+  Col,
+} from 'react-bootstrap';
 
 
 const formConfig = {
-  validation,
   initialValues: { email: '', password: '', githubUrl: '' },
+
+  validationSchema: yup.object().shape({
+    email: yup.string().required().email(),
+    password: yup.string().required().min(6),
+    githubUrl: yup.string().url().matches(/((https?:)?\/\/)?(www\.)?github\.com\/.+/i),
+  }),
 
   async onSubmit(values) {
     return axios
@@ -30,20 +35,72 @@ export default function RegisterPage() {
     <Container>
       <h2>Register</h2>
       <Formik {...formConfig}>
-        {({ isSubmitting }) => (
-          <Form method="post" action="/register">
-            <Field name="email" type="email" />
-            <ErrorMessage name="email" component="div" />
+        {({
+          values,
+          errors,
+          touched,
+          isSubmitting,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <Form method="post" action="/register" onSubmit={handleSubmit} noValidate>
+            <Form.Row>
 
-            <Field name="password" type="password" />
-            <ErrorMessage name="password" component="div" />
+              <Form.Group as={Col} md="6" controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  value={values.email}
+                  disabled={isSubmitting}
+                  isValid={touched.email && !errors.email}
+                  isInvalid={errors.email}
+                  onChange={handleChange}
+                />
+                {errors.email && (
+                  <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+                )}
+              </Form.Group>
 
-            <Field name="githubUrl" />
-            <ErrorMessage name="githubUrl" component="div" />
+              <Form.Group as={Col} md="6" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  value={values.password}
+                  disabled={isSubmitting}
+                  isValid={touched.password && !errors.password}
+                  isInvalid={errors.password}
+                  onChange={handleChange}
+                />
+                {errors.password && (
+                  <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
+                )}
+              </Form.Group>
 
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
+            </Form.Row>
+            <Form.Row>
+
+              <Form.Group as={Col} md="6" controlId="githubUrl">
+                <Form.Label>Github URL</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="githubUrl"
+                  value={values.githubUrl}
+                  disabled={isSubmitting}
+                  isValid={touched.githubUrl && !errors.githubUrl}
+                  isInvalid={errors.githubUrl}
+                  onChange={handleChange}
+                />
+                {errors.githubUrl && (
+                  <Form.Control.Feedback type="invalid">{errors.githubUrl}</Form.Control.Feedback>
+                )}
+              </Form.Group>
+
+            </Form.Row>
+
+            <Button type="submit">Register</Button>
+
           </Form>
         )}
       </Formik>
