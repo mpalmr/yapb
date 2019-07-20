6b713bbd0e31d44f27fd3d143e975eadf9982d7c
Matthew Palmer <matt@silvercreekit.com>
2019-07-20 17:17:25 -0400

    add code editor

--- components/pastebin/file.jsx
+++ components/pastebin/file.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
+import SimpleCodeEditor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
 
 
 function PastebinFile({ contents, remove, setContents }) {
   return (
     <div>
-      <input value={contents} onChange={event => setContents(event.target.value)} />
+      <SimpleCodeEditor
+        value={contents}
+        padding={10}
+        highlight={code => highlight(code, languages.js)}
+        onValueChange={setContents}
+        style={{
+          fontFamily: '"Fira code", "Fira Mono", monospace',
+          fontSize: 14,
+          border: '1px solid black',
+        }}
+      />
       <Button variant="danger" onClick={remove}>
         Remove
       </Button>
