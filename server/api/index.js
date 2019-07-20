d050e7b63ee9aaf94f0a7b93f114d8051803d738
Matthew Palmer <matt@silvercreekit.com>
2019-07-20 15:46:28 -0400

    only apply json middleware on API router

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { Router } = require('express');
+const express = require('express');
 const pasteRoute = require('./paste');
 const loginRoute = require('./login');
 const logoutRoute = require('./logout');
@@ -8,9 +8,10 @@ const registerRoute = require('./register');
 
 
 module.exports = function api({ server, db }) {
-  const router = Router();
-  const app = { router, db };
+  const router = express.Router();
+  router.use(express.json());
 
+  const app = { router, db };
   pasteRoute(app);
   loginRoute(app);
   logoutRoute(app);
--- server/index.js
+++ server/index.js
@@ -15,22 +15,22 @@ const errorMiddleware = require('./middleware/error');
 // Initialize Next.js
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
-const db = knex(knexConfig);
-const sessionStore = new KnexSessionStore({ knex: db });
+
 
 app
   .prepare()
   .then(() => {
     // Initialize Express and apply middleware
     const server = express();
+    const db = knex(knexConfig);
+
     server.use(helmet());
-    server.use(express.json());
 
     server.use(session({
       secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: true,
-      store: sessionStore,
+      store: new KnexSessionStore({ knex: db }),
       cookie: {
         maxAge: 1200000, // 20 minutes
         httpOnly: true,
@@ -41,7 +41,7 @@ app
     // Apply API routes
     api({ server, db });
 
-    // Default route handler
+    // Default route handlers
     server.get('*', handle);
     server.use(errorMiddleware);
 
