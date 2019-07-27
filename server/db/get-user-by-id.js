b3528ccfb626a72f1fae2d513875f156789f0200
Matthew Palmer <matt@silvercreekit.com>
2019-07-27 08:35:08 -0400

    inline db queries for user pastes

--- server/db/get-user-by-id.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = async function getUserById(db, userId) {
-  return db('users')
-    .select('uuid as id', 'email')
-    .where('uuid', '=', userId)
-    .first();
-};
--- server/endpoints/user-pastes.js
+++ server/endpoints/user-pastes.js
@@ -1,14 +1,51 @@
 'use strict';
 
-const getPastesByUser = require('../db/get-pastes-by-user');
-const getUserById = require('../db/get-user-by-id');
-
-
 module.exports = function userPastesEndpoint({ pageRouter, apiRouter, db }) {
+  async function getPastesByUser(userId) {
+    return db('pastes')
+      .select(
+        'pastes.uuid as paste_id',
+        'pastes.created_at',
+        'pastes.updated_at',
+        'files.uuid as file_id',
+        'files.name',
+        'files.contents',
+        'files.updated_at as file_updated_at',
+      )
+      .innerJoin('files', 'pastes.id', '=', 'files.paste_id')
+      .innerJoin('users', 'pastes.user_id', '=', 'users.id')
+      .where('users.uuid', '=', userId)
+      .then(records => Object.entries(records
+        .reduce((acc, { pasteId, ...record }) => ({
+          ...acc,
+          [pasteId]: {
+            id: pasteId,
+            createdAt: record.createdAt,
+            updatedAt: record.updatedAt,
+            files: ((acc[pasteId] || {}).files || []).concat({
+              id: record.fileId,
+              name: record.name,
+              contents: record.contents,
+              updatedAt: record.fileUpdatedAt,
+            }),
+          },
+        }), {}))
+        .map(([id, paste]) => ({ ...paste, id })));
+  }
+
+
+  async function getUserById(userId) {
+    return db('users')
+      .select('uuid as id', 'email')
+      .where('uuid', '=', userId)
+      .first();
+  }
+
+
   async function query(req, res, next) {
     return Promise.all([
-      getPastesByUser(db, req.params.id),
-      getUserById(db, req.params.id),
+      getPastesByUser(req.params.id),
+      getUserById(req.params.id),
     ])
       .then(([pastes, user]) => {
         res.locals = {
