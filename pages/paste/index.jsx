73d3f1aa86e21ee8576015a88db1be8d80cdd13f
Matthew Palmer <matt@silvercreekit.com>
2019-07-27 00:01:32 -0400

    show current user pastes

--- pages/paste/index.jsx
+++ pages/paste/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from 'styled-jsx/css';
+import { CardDeck, Card, Button } from 'react-bootstrap';
 import client from '../../client';
+
+
+const cardFooterCss = css.resolve`
+  text-align: right;
+`;
 
 
 function CurrentUserPastes(props) {
@@ -18,9 +25,40 @@ function CurrentUserPastes(props) {
   return (
     <>
       <h1>Your Pastes</h1>
-      {pastes.map(paste => (
-        <p key={paste.id}>{paste.id}</p>
-      ))}
+
+      {pastes.length ? (
+        <CardDeck as="ul">
+          {pastes.map(paste => (
+            <Card key={paste.id} as="li">
+              <Card.Body>
+                <Card.Title>Placeholder name</Card.Title>
+                <Card.Text>Created on: {paste.createdAt.toLocaleString()}</Card.Text>
+
+                {paste.files.length} files:
+                <ul>
+                  {paste.files.map(file => (
+                    <li key={file.id}>
+                      {file.name}
+                    </li>
+                  ))}
+                </ul>
+              </Card.Body>
+
+              <Card.Footer className={cardFooterCss.className}>
+                <Button href={`/paste/${paste.id}`} variant="info" size="sm">
+                  View
+                </Button>
+              </Card.Footer>
+            </Card>
+          ))}
+        </CardDeck>
+      ) : (
+        <p className="no-pastes-message">
+          You have not created any pastes yet.
+        </p>
+      )}
+
+      {cardFooterCss.styles}
     </>
   );
 }
