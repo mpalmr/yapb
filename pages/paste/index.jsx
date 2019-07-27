29b7c31eb0e1bfa699b9a7a1db297d059d27abfd
Matthew Palmer <matt@silvercreekit.com>
2019-07-26 21:23:51 -0400

    parse pastes client side

--- pages/paste/index.jsx
+++ pages/paste/index.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import client from '../../client';
 
 
 function CurrentUserPastes(props) {
-  console.log(props);
+  const pastes = props.pastes.map(paste => ({
+    ...paste,
+    createdAt: new Date(paste.createdAt),
+    updatedAt: new Date(paste.updatedAt),
+    files: paste.files.map(file => ({
+      ...file,
+      updatedAt: new Date(file.updatedAt),
+    })),
+  }));
+
+
   return (
-    <p>Pastes</p>
+    <>
+      <h1>Your Pastes</h1>
+      {pastes.map(paste => (
+        <p key={paste.id}>{paste.id}</p>
+      ))}
+    </>
   );
 }
+
+
+CurrentUserPastes.propTypes = {
+  pastes: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    updatedAt: PropTypes.string.isRequired,
+    files: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      contents: PropTypes.string.isRequired,
+      updatedAt: PropTypes.string.isRequired,
+    })).isRequired,
+  })).isRequired,
+};
 
 
 CurrentUserPastes.getInitialProps = async function ({ res }) {
